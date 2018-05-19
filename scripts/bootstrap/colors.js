10f01cd232400da4b64ae21336ed1a3e2989aa02
Judd Younce <jyounce@yahoo.com>
2018-05-18 20:28:26 -0500

    chore(travis): enable travis for continuous integration and automated publishing to npm

---
+++ scripts/bootstrap/colors.js
@@ -0,0 +1,12 @@
+/*******************
+ * BOOTSTRAP COLORS
+ *******************/
+require('colors').setTheme({
+	alert:   'yellow',
+	attn:    ['cyan', 'bold'],
+	error:   ['red', 'bold'],
+	info:    'cyan',
+	minor:   'grey',
+	success: ['green', 'bold'],
+	warn:    'magenta'
+});
