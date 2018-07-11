cbf5a1692a176388bc00ad8bc151a1b2e55cee3f
Rapid Build UI <rapidbuildui@yahoo.com>
2018-07-10 23:38:03 -0500

    chore(add): EventService and viewReady() event but currently don't use

--- src/client/scripts/renderer.js
+++ src/client/scripts/renderer.js
@@ -6,6 +6,10 @@ import { html, render } from '../../../lit-html/lib/lit-extended.js';
 const withRenderer = (Base = HTMLElement) => class extends Base {
 	renderer(root, call) {
 		render(call(), root);
+		// custom only executed after initial render
+		if (this.isViewReady || !this.isConnected) return;
+		this.isViewReady = true;
+		this.viewReady && this.viewReady();
 	}
 };
 
