ef5dcf4de53dbad9582f168a57f2223cf821484c
yokun <yevgeniyokun@gmail.com>
2018-08-17 15:09:18 -0500

    feat(validation): add support for native form validation

--- src/client/scripts/rb-input.js
+++ src/client/scripts/rb-input.js
@@ -4,9 +4,10 @@
 import { props, withComponent } from '../../../skatejs/dist/esnext/index.js';
 import { html, withRenderer } from './renderer.js';
 import EventService from './event-service.js';
-import validate from './validation.js';
+import Validation from './validation/validation.js';
 import type from './type.js';
-import validationMessages from './validation-messages.js';
+
+import validationMessages from './validation/messages.js';
 import '../../rb-icon/scripts/rb-icon.js';
 import template from '../views/rb-input.html';
 
@@ -16,6 +17,11 @@ export class RbInput extends withComponent(withRenderer()) {
 	constructor() {
 		super();
 		this.rbEvent = EventService.call(this);
+		Validation.onFormSubmit.call(this);
+	}
+
+	viewReady() {
+		Validation.append.call(this)
 	}
 
 	/* Properties
@@ -29,6 +35,9 @@ export class RbInput extends withComponent(withRenderer()) {
 			inline: props.boolean,
 			kind: props.string,
 			label: props.string,
+			name: Object.assign({}, props.string, {
+				default: Math.round((Math.random() * 36 ** 5)).toString(36)
+			}),
 			placeholder: props.string,
 			right: props.boolean,
 			subtext: props.string,
@@ -48,6 +57,7 @@ export class RbInput extends withComponent(withRenderer()) {
 		}
 	}
 
+
 	/* Observer
 	 ***********/
 	updating(prevProps) { // :void
@@ -62,6 +72,12 @@ export class RbInput extends withComponent(withRenderer()) {
 	_onfocus(e) {
 		this._active = true;
 	}
+
+	_onchange(e) {
+		if (!this._hiddenInput) return;
+		this._hiddenInput.value = this.value;
+	}
+
 	async _oninput(e) { // TODO: add debouncing
 		const oldVal = this.value;
 		const newVal = e.target.value;
@@ -69,57 +85,15 @@ export class RbInput extends withComponent(withRenderer()) {
 		if (!this._dirty && newVal !== oldVal)
 			return this._dirty = true;
 		if (!this._blurred) return;
-		if (!this.validation.length) return;
-		await this._validate();
+
+		await Validation.validate.call(this);
 	}
 	async _onblur(e) {
 		this._active = false;
 		if (!this._dirty) return;
 		this._blurred = true;
 		this.value = e.target.value;
-		if (!this.validation.length) return;
-		await this._validate();
-	}
-
-	/* Validation
-	 *************/
-	async _validate() { // :void
-		let valid = true;
-		for (const validator of this.validation) {
-			if (!validator) break;
-			if (!valid) break;
-			switch(true) {
-				case type.is.function(validator):
-					valid = await this._validateCustom(validator)
-					break;
-				case type.is.object(validator):
-					valid = this._validateObject(validator);
-					break;
-				default:
-					valid = this._validateSimple(validator);
-			}
-		}
-		if (valid) this._eMsg = '';
-		this._valid = valid;
-	}
-
-	_validateSimple(validator) { // :boolean
-		const out = validate[validator](this.value);
-		if (!out.valid) this._eMsg = out.message;
-		return out.valid;
-	}
-
-	_validateObject(validator) { // :boolean
-		const key = Object.keys(validator)[0]
-		const out = validate[key](this.value, validator[key]);
-		if (!out.valid) this._eMsg = out.message;
-		return out.valid;
-	}
-
-	async _validateCustom(validator) { // :boolean (validator is function)
-		let out = await validator(this.value);
-		if (!out.valid) this._eMsg = out.message;
-		return out.valid;
+		await Validation.validate.call(this);
 	}
 
 	/* Template
